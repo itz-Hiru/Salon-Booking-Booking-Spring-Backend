46dadf941e31060b0b1555a4ed1980465efff2b5
itz-Hiru <hirumithagithub@gmail.com>
2025-04-07 17:25:08 +0530

    Created simple springboot project

---
+++ src/test/java/com/hiru/salon/booking/BookingMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiru.salon.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
